348aec8862136254dc42b64140eb59e4887f636a
JulenRa-dev <134146236+JulenRa-dev@users.noreply.github.com>
2023-08-10 12:22:49 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'Discord easy commands plus'
+LONG_DESCRIPTION = 'Discord easy commands plus, a library to make discord bots in a easy way'
+
+# Configurando
+setup(
+       # el nombre debe coincidir con el nombre de la carpeta 	  
+       #'modulomuysimple'
+        name= "discord_easy_commands_plus", 
+        version=VERSION,
+        author="JRaDev",
+        author_email="<tuemail@email.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=["discord","flask","threading"], # añade cualquier paquete adicional que debe ser
+        #instalado junto con tu paquete. Ej: 'caer'
+        
+        keywords=['python', 'primer paquete'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
