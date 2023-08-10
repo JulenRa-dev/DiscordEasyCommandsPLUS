1e4bfedcb6244f08c199e0155389a551a6935244
JulenRa-dev <134146236+JulenRa-dev@users.noreply.github.com>
2023-08-10 12:25:31 +0200

    Update exampleOfUsage.py

--- discord_easy_commands_plus/exampleOfUsage.py
+++ discord_easy_commands_plus/exampleOfUsage.py
@@ -6,7 +6,8 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = EasyBotPlus(intents=intents)
-bot.setCommand("!help",["You can use !hello to make me say hello", "Or you can use !goodbye to make me say goodbye"])
-bot.setCommand("!hello",["Hello"])
-bot.setCommand("!goodbye",["Goodbye"])
+bot.setCommand("!help",["mYou can use !hello to make me say hello", "Or you can use !goodbye to make me say goodbye"])
+bot.setCommand("!hello",["mHello"])
+bot.setCommand("!goodbye",["mGoodbye"])
+bot.setCommand("!startDm",["wHello at DM"]
 bot.run(os.environ["TOKEN"]) #This part of "os.environ["TOKEN"] is because this must run on replit, and you must add your token as a Secret
